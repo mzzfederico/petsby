a6c503ea4dd70f33f2727293507018824ae21d3b
Federico Muzzo <muzzofederico@outlook.com>
2020-03-29 05:44:53 +0200

    Home

--- src/pages/search.js
+++ src/pages/search.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import SearchBlock from "../components/search";
+import ResultsBlock from "../components/results";
 
 const IndexPage = () => (
     <Layout>
-        <SEO title="Home" />
+        <SEO title="Search" />
         <SearchBlock />
+        <ResultsBlock />
     </Layout>
 );
 
